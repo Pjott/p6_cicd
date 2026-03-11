19bebabb6f72dfccdaf17c1ceb039cee9df4edf5
Piotr <piotr@MacBook-Air-Piotr.local>
2026-03-11 21:51:09 +0100

    added remote state variables

--- providers.tf
+++ providers.tf
@@ -5,7 +5,12 @@ terraform {
       version = "~> 3.0"
     }
   }
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = var.rs_resource_group_name
+    storage_account_name = var.rs_storage_account_name
+    container_name       = var.rs_container_name
+    key                  = "prod.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,4 @@
 resource_group_name = "rg-cicd-test"
 azure_region = "westeurope"
+#remote state variables
+rs_resource_group_name = "rg-terraform-state"
