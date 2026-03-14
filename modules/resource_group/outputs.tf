bbd02ea0a5b820e477e56e21985aecdda64f30ad
Piotr <piotr@MacBook-Air-Piotr.local>
2026-03-14 21:31:41 +0100

    fix- added missing output

--- modules/resource_group/outputs.tf
+++ modules/resource_group/outputs.tf
@@ -2,3 +2,8 @@ output "rg_id" {
   value = azurerm_resource_group.rg.id
   description = "resource group ID"
 }
+
+output "rg_name" {
+  value       = azurerm_resource_group.rg.name
+  description = "rg name"
+}
