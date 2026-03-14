d4f3170ffaeabddcde8afc586452454d37f40376
Piotr <piotr@MacBook-Air-Piotr.local>
2026-03-14 21:58:08 +0100

    fix- aks module- added dns prefix and fixed name pool

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -2,9 +2,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
   resource_group_name   = var.rg_name
   location              = var.location
   name                  = var.aks_name
-
+  dns_prefix          = "${var.aks_name}-dns"
+  
   default_node_pool {
-    name = "default-node-pool"
+    name = "default"
     node_count = 1
     vm_size = "Standard_B2s"
   }
